11047ef1e76dc4a31920c88639e4bd33e34c899f
Fidel Taveras <fideltaveras@Fidels-MacBook-Pro-2.local>
2025-10-01 01:19:19 -0400

    Properly identify categories and prices

--- shopping-cart/src/components/Filters.jsx
+++ shopping-cart/src/components/Filters.jsx
@@ -1,18 +1,42 @@
 import './Filters.css'
-export function Filters () {
+import { useId, useState } from 'react'    
+export function Filters ({ onChange }) {
+    const [minPrice, setMinPrice] = useState(0)
+    const minPriceFilterId = useId()
+    const categoryFilterId = useId()
+
+    const handleChangeMinPrice = (event) => {
+        setMinPrice(event.target.value)
+        onChange(prevState => ({
+            ...prevState,
+            minPrice: event.target.value,
+        }))
+    }
+
+    const handleChangeCategory = (event) => {
+        onChange(prevState => ({
+            ...prevState,
+            category: event.target.value,
+        }))
+    }
+
   return (
     <section className="filters">
         <div>
-            <label htmlFor="price">Price</label>
+            <label htmlFor={minPriceFilterId}>Price</label>
             <input 
                 type="range" 
-                id="price" 
-                min="0" 
+                id={minPriceFilterId} 
+                min="0"
                 max="1000"
+                onChange={handleChangeMinPrice}
                 />
+
+            <span>Minimun price: ${minPrice}</span>
+
                 <div>
-                    <label htmlFor="category">Category</label>
-                    <select id="category" name="category">
+                    <label htmlFor={categoryFilterId}>Category</label>
+                    <select id={categoryFilterId} onChange={handleChangeCategory}>
                         <option value="all">All</option>
                         <option value="smartphones">Smartphones</option>
                         <option value="laptops">Laptops</option>
